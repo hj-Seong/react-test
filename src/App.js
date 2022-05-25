aecb879f498ee51780a1b74f40d728e4b698698c
hjseong <hjseong1222@gmail.com>
2022-05-25 11:10:52 +0900

    react-test-hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MemoClass from "./components/MemoClass";
 import MemoFunc from "./components/MemoFunc";
+import TestHook from "./components/TestHook";
 
 function App() {
   return (
     <div className="App">
+      <TestHook />
       <MemoFunc />
       <MemoClass />
     </div>
